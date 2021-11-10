4e18ace30fbafe4ac8b77fffda43e4f5451f0e90
Արմեն Բադալյան <armenbadal@gmail.com>
2021-11-10 16:32:21 +0400

    Ավելացնում է օրինակների նախնական ֆայլերը

---
+++ code/hello.cxx
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "Ողջո՜ւյն, աշխարհ։" << std::endl;
+
+  return 0;
+}
---
+++ code/hello2.cxx
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::cout << "Ներմուծեք Ձեր անունը. " << std::endl;
+  std::string name;
+  std::cin >> name;
+  std::cout << "Ողջո՜ւյն, " << name << "։" << std::endl;
+
+  return 0;
+}
