a74c0a041005bf649c85e57d101b4869ad44c2e3
Արմեն Բադալյան <armenbadal@gmail.com>
2022-06-24 09:26:12 +0400

    Կառուցվածքի փոփոխություններ

--- code/implication.cxx
+++ code/implication.cxx
@@ -6,8 +6,8 @@ int main()
     std::cout << "Ներմուծեք a-ի և b-ի արժեքները. ";
     std::cin >> ac >> bc;
 
-    const bool a = ac == 'T'
-    const bool b = ac == 'T';
+    const bool a = ac == 'T';
+    const bool b = bc == 'T';
 
     const bool a_imp_b = (!a && !b) || (!a && b) || (a && b);
     std::cout << ac << " ⇒ " << bc << " = " << a_imp_b << std::endl;
